6bbb098b603472cc13b0c122838d1e8c8e19aa5a
Avi <apoorv.r02@gmail.com>
2024-09-10 18:45:07 -0500

    finished

--- Dicee+Challenge+-+Starting+Files/index.js
+++ Dicee+Challenge+-+Starting+Files/index.js
@@ -5,3 +5,12 @@ var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 // Set the src for both img1 and img2
 document.querySelector(".img1").src = `./images/dice${randomNumber1}.png`;
 document.querySelector(".img2").src = `./images/dice${randomNumber2}.png`;
+
+if (randomNumber1>randomNumber2)
+    var winner= "Winner: Player 1"
+if (randomNumber1<randomNumber2)
+    var winner= "Winner: Player 2"
+else
+    var winner= "Draw"
+
+document.querySelector("h1").innerHTML = winner;
