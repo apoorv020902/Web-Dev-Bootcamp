2a15f653ae92942515fa3ac5417d6fb0d4c0bd3e
Avi <apoorv.r02@gmail.com>
2024-09-26 21:36:48 -0500

    added animation for both click and key presses; basics completed;

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -6,12 +6,14 @@ for(i=0; i<numberOfDrumButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     var buttonInnerHTML = this.innerHTML;
     playSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
     });
 }
 
 //Detecting Button Press
 document.addEventListener('keydown', function(event){
     playSound(event.key);
+    buttonAnimation(event.key);
 })
 
 //Different Sound for Different Characters Passed to the function
@@ -59,4 +61,13 @@ function playSound(char) {
             break;
     
     }
+}
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("."+currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    }, 100);
 }
