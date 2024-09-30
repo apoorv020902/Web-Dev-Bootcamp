a553565d559ef1838a139f6116ecb577032b1714
Avi <apoorvrana@Apoorvs-MacBook-Air.local>
2024-09-29 19:49:57 -0500

    debug if stament

--- Dicee+Challenge+-+Starting+Files/index.js
+++ Dicee+Challenge+-+Starting+Files/index.js
@@ -6,11 +6,14 @@ var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 document.querySelector(".img1").src = `./images/dice${randomNumber1}.png`;
 document.querySelector(".img2").src = `./images/dice${randomNumber2}.png`;
 
-if (randomNumber1>randomNumber2)
-    var winner= "Winner: Player 1"
-if (randomNumber1<randomNumber2)
-    var winner= "Winner: Player 2"
-else
-    var winner= "Draw"
+// Determine the winner
+var winner;
+if (randomNumber1 > randomNumber2) {
+    winner = "Winner: Player 1";
+} else if (randomNumber1 < randomNumber2) {
+    winner = "Winner: Player 2";
+} else {
+    winner = "Draw";
+}
 
 document.querySelector("h1").innerHTML = winner;
